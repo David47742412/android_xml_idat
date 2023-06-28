83fede2aab6d25a082ae9b74a7ac8d0112f987ad
qumadev <adrian.mamani.dev@gmail.com>
2023-06-27 19:14:26 -0500

    estructura

--- app/src/main/java/com/tuttobello/tuttobello/MainActivity.java
+++ app/src/main/java/com/tuttobello/tuttobello/MainActivity.java
@@ -3,44 +3,54 @@ package com.tuttobello.tuttobello;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.tuttobello.tuttobello.room.dao.UserDao;
 import com.tuttobello.tuttobello.room.database.__AppDatabase__;
 import com.tuttobello.tuttobello.room.model.User;
+//////////////////
+
+import android.os.Handler;
+import android.os.Looper;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ExecutorService executor;
+    private Handler handler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        new DatabaseTask().execute();
-    }
 
-    private class DatabaseTask extends AsyncTask<Void, Void, User> {
-        @Override
-        protected User doInBackground(Void... voids) {
-            // Crear la instancia de la base de datos
-            __AppDatabase__ database = Room.databaseBuilder(getApplicationContext(),
-                    __AppDatabase__.class, "AppDatabase").build();
+        executor = Executors.newSingleThreadExecutor();
+        handler = new Handler(Looper.getMainLooper());
 
-            // Obtener el UserDao
-            UserDao userDao = database.userDao();
+        executor.execute(new Runnable() {
+            @Override
+            public void run() {
+                // Tareas en segundo plano aquí
 
-            // Realizar la consulta en segundo plano
-            return userDao.findOne("david@gmail.com");
-        }
 
-        @Override
-        protected void onPostExecute(User user) {
-            if (user != null) {
-                Log.e("--ejecución", user.email);
-            } else {
-                Log.e("--ejecución", "Usuario no encontrado");
+
+                handler.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // Tareas en el hilo principal (UI) aquí
+
+                    }
+                });
             }
-        }
+        });
     }
+
 }

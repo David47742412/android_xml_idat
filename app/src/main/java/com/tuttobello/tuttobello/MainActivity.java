241c4d9835854fbb60b3bc9dd2c6bcd539387975
David <davidcastroastudillo20@gmail.com>
2023-07-18 19:48:52 -0500

    c

--- app/src/main/java/com/tuttobello/tuttobello/MainActivity.java
+++ app/src/main/java/com/tuttobello/tuttobello/MainActivity.java
@@ -1,7 +1,6 @@
 package com.tuttobello.tuttobello;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.room.Room;
 
 import android.os.Bundle;
 import android.util.Log;
@@ -9,7 +8,6 @@ import android.util.Log;
 import com.tuttobello.tuttobello.room.dao.UserDao;
 import com.tuttobello.tuttobello.room.database.__AppDatabase__;
 import com.tuttobello.tuttobello.room.model.User;
-//////////////////
 
 import android.os.Handler;
 import android.os.Looper;
@@ -23,34 +21,25 @@ import java.util.concurrent.Executors;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ExecutorService executor;
-    private Handler handler;
+private ExecutorService executor;
+private Handler handler;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
-        executor = Executors.newSingleThreadExecutor();
-        handler = new Handler(Looper.getMainLooper());
-
-        executor.execute(new Runnable() {
-            @Override
-            public void run() {
-                // Tareas en segundo plano aquí
-
-
-
-                handler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        // Tareas en el hilo principal (UI) aquí
-
-                    }
-                });
-            }
-        });
-    }
-
+@Override
+protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	setContentView(R.layout.login);
+	
+	executor = Executors.newSingleThreadExecutor();
+	handler = new Handler(Looper.getMainLooper());
+	
+	executor.execute(() -> {
+		// Tareas en segundo plano aquí
+		
+		handler.post(() -> {
+			// Tareas en el hilo principal (UI) aquí
+			
+		});
+	});
+}
 }

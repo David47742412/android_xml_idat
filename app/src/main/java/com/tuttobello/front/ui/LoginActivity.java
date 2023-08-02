c9d51039e7b56410a70516a73115d9b526d57de5
David <davidcastroastudillo20@gmail.com>
2023-08-01 20:34:59 -0500

    view

--- app/src/main/java/com/tuttobello/front/ui/LoginActivity.java
+++ app/src/main/java/com/tuttobello/front/ui/LoginActivity.java
@@ -10,14 +10,11 @@ import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.snackbar.Snackbar;
 import com.tuttobello.front.R;
 import com.tuttobello.front.model.auth.RAuth;
 import com.tuttobello.front.model.auth.SAuth;
 import com.tuttobello.front.model.response.ResponseApi;
-import com.tuttobello.front.room.dao.main.IUserDao;
-import com.tuttobello.front.room.entites.main.UserEntity;
-import com.tuttobello.front.room.helper.main.DbMainHelper;
-import com.tuttobello.front.room.service.UserService;
 import com.tuttobello.front.usecase.LoginUseCase;
 
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
@@ -28,49 +25,49 @@ import io.reactivex.rxjava3.schedulers.Schedulers;
 
 public class LoginActivity extends AppCompatActivity {
 
-private final OnClickListener onLogin = (v) -> {
-	Context CONTEXT = LoginActivity.this;
-	try {
-		EditText username = findViewById(R.id.etUsername);
-		EditText password = findViewById(R.id.etPassword);
-		LoginUseCase loginUseCase = new LoginUseCase();
-		SAuth auth = new SAuth(username.getText().toString(), password.getText().toString());
-		loginUseCase.login("auth/login", auth)
-				.subscribeOn(Schedulers.io())
-				.observeOn(AndroidSchedulers.mainThread())
-				.subscribe(new SingleObserver<ResponseApi<RAuth>>() {
-					@Override
-					public void onSubscribe(@NonNull Disposable d) {
-					
-					}
-					
-					@Override
-					public void onSuccess(@NonNull ResponseApi<RAuth> response) {
-						if (response.statusCode == 401) {
-							Toast.makeText(CONTEXT, "No autorizado", Toast.LENGTH_LONG).show();
-							return;
+	private final OnClickListener onLogin = (v) -> {
+		Context CONTEXT = LoginActivity.this;
+		try {
+			EditText username = findViewById(R.id.etUsername);
+			EditText password = findViewById(R.id.etPassword);
+			LoginUseCase loginUseCase = new LoginUseCase();
+			SAuth auth = new SAuth(username.getText().toString(), password.getText().toString());
+			loginUseCase.login("auth/login", auth)
+					.subscribeOn(Schedulers.io())
+					.observeOn(AndroidSchedulers.mainThread())
+					.subscribe(new SingleObserver<ResponseApi<RAuth>>() {
+						@Override
+						public void onSubscribe(@NonNull Disposable d) {
+						
 						}
-						Intent onHome = new Intent(LoginActivity.this, HomeActivity.class);
-						startActivity(onHome);
-						finish();
-					}
-					
-					@Override
-					public void onError(@NonNull Throwable e) {
-						Toast.makeText(CONTEXT, "Ha Ocurrido un Error al momento de autenticar", Toast.LENGTH_LONG).show();
-					}
-				});
-	} catch (Exception ex) {
-		Toast.makeText(CONTEXT, ex.getMessage(), Toast.LENGTH_LONG).show();
-	}
-};
+						
+						@Override
+						public void onSuccess(@NonNull ResponseApi<RAuth> response) {
+							if (response.statusCode == 401) {
+								Snackbar.make(v, "Usuario o contraseña incorrectos", Snackbar.LENGTH_LONG).show();
+								return;
+							}
+							Intent onHome = new Intent(CONTEXT, HomeActivity.class);
+							startActivity(onHome);
+							finish();
+						}
+						
+						@Override
+						public void onError(@NonNull Throwable e) {
+							Snackbar.make(v, "Ha Ocurrido un Error al momento de autenticar", Snackbar.LENGTH_LONG).show();
+						}
+					});
+		} catch (Exception ex) {
+			Toast.makeText(CONTEXT, ex.getMessage(), Toast.LENGTH_LONG).show();
+		}
+	};
 
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-	super.onCreate(savedInstanceState);
-	setContentView(R.layout.activity_login);
-	Button btnLogin = findViewById(R.id.btnLogin);
-	btnLogin.setOnClickListener(onLogin);
-}
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+		Button btnLogin = findViewById(R.id.btnLogin);
+		btnLogin.setOnClickListener(onLogin);
+	}
 
 }
